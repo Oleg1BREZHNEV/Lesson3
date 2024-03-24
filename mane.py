9ee0baaf654e2ea8ec894d326b2934d667599549
Oleg1BREZHNEV <142392074+Oleg1BREZHNEV@users.noreply.github.com>
2024-03-24 22:07:33 +0300

    add const

--- mane.py
+++ mane.py
@@ -6,8 +6,12 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(«Игра Тир»)
+<<<<<<< Updated upstream
 icon = pygame.image.load (<<img/king-konga-instagram-48.jpg>>)
 pygame.display.set_icon(icon)
+=======
+icon = pygame.image.load(img/)
+>>>>>>> Stashed changes
 
 
 target_width = 50
